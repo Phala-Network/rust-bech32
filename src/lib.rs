7a142a676e83e647416a679af4ccfcfb6ed29a3d
Shelven Zhou <favoring.zhou49@gmail.com>
2021-04-21 14:38:11 +0800

    Port to sgx 1.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -52,8 +52,17 @@
 #![deny(unused_mut)]
 #![cfg_attr(feature = "strict", deny(warnings))]
 
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
 use std::{error, fmt};
 use std::borrow::Cow;
+use std::string::String;
+use std::vec::Vec;
 
 // AsciiExt is needed for Rust 1.14 but not for newer versions
 #[allow(unused_imports, deprecated)]
